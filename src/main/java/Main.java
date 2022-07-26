3f450e344cf0c06ba57f5f6200de61c6702d7763
Abdella <abdella.shukur2@gmail.com>
2022-07-26 13:02:28 -0400

    thread interference has been resolved

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,13 +6,17 @@ class Main {
 
         Thread t1 = new Thread(() -> IntStream.range(0, 10_000_000).forEach((i) -> counter.inc()));
         Thread t2 = new Thread(() -> IntStream.range(0, 30_000_000).forEach((i) -> counter.inc()));
-        
-        t1.start();
-        t2.start();
 
+        // Since both threads are using the same value of counter instance,
+        // There is thread interference
+
+        t1.start();
         t1.join();
+        t2.start();
         t2.join();
 
+        // Terminated the first thread before the 2nd thread starts will solve issue
+        
         System.out.println(counter.getCount());
     }
 }
